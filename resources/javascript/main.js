151bd1fb2bf76f4c0cefabfc7d1b53258d148239
KvDread <97404672+KvDread@users.noreply.github.com>
2022-11-12 23:36:08 +0100

    Add defer to FA script

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -13,12 +13,21 @@ function mobileMenu(){
         }else {
             overlay.style.display = "none";
         }
+
+        // overlay.style.display === "block" ? overlay.style.display = 'none' : overlay.style.display = 'block';
+        
         const overflowProp = ['scroll', 'auto', 'visible', ''];
+        
+        // overflowProp.includes(body.style.overflowY) ? body.style.overflowY = 'hidden' : body.style.overflowY = 'scroll';
+        
         if (overflowProp.includes(body.style.overflowY)) {
             body.style.overflowY = 'hidden';
         } else {
             body.style.overflowY = 'scroll';
         }
+        
+        // overflowProp.includes(body.style.overflowY) ? body.style.overflowY = 'hidden' : body.style.overflowY = 'scroll';
+
         navBarMenu.classList.toggle('displayMobileMenu');
 }
 
